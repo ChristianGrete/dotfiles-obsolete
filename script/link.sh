92e2b2d00712a82b4d097369df0f2889143bb6ef
Christian Grete <webmaster@christiangrete.com>
2016-06-24 11:48:25 +0200

    feat(script): add script [WIP]

---
+++ script/link.sh
@@ -0,0 +1,35 @@
+start=`expr ${#DOTFILES} + 2`
+
+for target in `walk $DOTFILES`
+  do
+    base=`echo $target | cut -c $start-${#target} | grep -vf $PROJECT/.dotfilesignore`
+
+    if [ -z ${base:+_} ]
+      then
+        continue
+    fi
+
+    dir=`dirname "$base"`
+    link=$HOME/$base
+
+    unset base
+
+    if [ "$dir" != . ]
+      then
+        dir=$HOME/$dir
+
+        if [ ! -e "$dir" ]
+          then
+            mkdir -p "$dir"
+        fi
+    fi
+
+    unset dir
+
+    symlink $target "$link"
+
+    unset link
+done
+
+unset start
+unset target

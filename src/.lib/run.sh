462e6647180e8c9f834ab1c2abad1dcd58c82fa6
Christian Grete <webmaster@christiangrete.com>
2016-06-29 11:07:19 +0200

    feat(lib): add `run` function - also add executable

---
+++ src/.lib/run.sh
@@ -0,0 +1,23 @@
+# POSIX compliant function to run executable files
+run () (
+  file="$(pwd)"
+
+  if [ -d "$file/bin" ] && [ -r "$file/bin" ] && [ -x "$file/bin/$1" ]
+    then
+      file="$file/bin/$1"
+  elif [ -d "$file/.bin" ] && [ -r "$file/.bin" ] && [ -x "$file/.bin/$1" ]
+    then
+      file="$file/.bin/$1"
+  elif [ -x "$file/$1" ]
+    then
+      file="$file/$1"
+  else
+    echo "$1: file not found or not executable" >&2
+
+    return 127
+  fi
+
+  shift 1
+
+  $file $@
+)

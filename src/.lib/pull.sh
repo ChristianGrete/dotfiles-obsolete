9f34fba53668513e60e669f923b718e905222662
Christian Grete <webmaster@christiangrete.com>
2016-08-25 10:23:28 +0200

    refactor(lib): rebase per default

--- src/.lib/pull.sh
+++ src/.lib/pull.sh
@@ -1,4 +1,4 @@
 # POSIX compliant function to update Git local refs
 pull () {
-  command git pull -v "${2:-origin}" "${1:-$(branch)}" 2>/dev/null
+  command git pull -vr "${2:-origin}" "${1:-$(branch)}" 2>/dev/null
 }

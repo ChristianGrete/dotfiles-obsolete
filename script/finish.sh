b704b7d981564aae87c3016570afaa9c83bd840f
Christian Grete <webmaster@christiangrete.com>
2017-03-20 16:34:28 +0100

    refactor(script): delete

--- script/finish.sh
+++
@@ -1,2 +0,0 @@
-echo 'Dotfiles successfully deployed!'
-echo
--- script/setup.sh
+++
@@ -1,9 +0,0 @@
-if [ -z ${PWD:+PWD} ]
-  then
-    export PWD="$(pwd)"
-fi
-
-export PROJECT="$PWD"
-export DOTFILES_ARTIFACT="$PROJECT/dist"
-export DOTFILES_LIBRARY="$DOTFILES_ARTIFACT/.lib"
-export SCRIPTS="$PROJECT/script"

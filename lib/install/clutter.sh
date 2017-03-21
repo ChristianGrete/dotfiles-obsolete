6cb6eaef7fa58aedfbd7a446960890f0ae52d4b7
Christian Grete <webmaster@christiangrete.com>
2017-03-21 14:50:13 +0100

    feat(lib): add temporary routine

---
+++ lib/install/clutter.sh
@@ -0,0 +1,24 @@
+printf 'Locating Oh My Zsh ... '
+
+if [ -d "${ZSH:=$HOME/.oh-my-zsh}" ]
+  then
+    cd "$ZSH"
+
+    echo 'success.'
+
+    echo 'Updating...'
+
+    git pull -vr --stat origin master
+
+    cd "$DOTFILES"
+else
+  echo 'not found.'
+
+  git clone git://github.com/robbyrussell/oh-my-zsh.git "$ZSH"
+
+  if [ $? -ne 0 ]
+    then
+      echo; echo 'Error: Unable to install Oh My Zsh.' >&2
+      exit 1
+  fi
+fi
